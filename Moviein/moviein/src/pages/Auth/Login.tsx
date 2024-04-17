30a87bfbb9760d92d07788d6196bba3a4deed76c
eduardo-oliveira2077 <eduardolimaoliveira@souunisuam.com.br>
2024-04-17 10:48:50 -0300

    imagem ocupar a tela

--- Moviein/moviein/src/pages/Auth/Login.tsx
+++ Moviein/moviein/src/pages/Auth/Login.tsx
@@ -10,7 +10,7 @@ const Login: React.FC = () => {
 
   return (
     <div className='relative w-full h-screen'>
-      <img src={filmes} alt='Filmes' className='absolute w-full h-full object-cover -z-10' />
+      <img src={filmes} alt='Filmes' className='absolute w-full h-screen object-cover -z-10' />
       <div className='container mx-auto'>
         <div className='grid md:grid-cols-2 grid-cols-1 h-screen'>
           <div className='md:col-start-2'>
