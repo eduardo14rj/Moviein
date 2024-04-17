153910a7c8fbd85709d5f40f31693d9071030f76
eduardo-oliveira2077 <eduardolimaoliveira@souunisuam.com.br>
2024-04-17 10:40:23 -0300

    Update Login.tsx

--- Moviein/moviein/src/pages/Auth/Login.tsx
+++ Moviein/moviein/src/pages/Auth/Login.tsx
@@ -17,15 +17,14 @@ const Login: React.FC = () => {
             <div className='py-12 h-full'>
               <div className='bg-dark rounded-md p-8 flex flex-col h-full relative'>
                 <div className='mb-4'>
-                  <img src={logo} className='w-[120px]' />
+                  <img src={logo} alt='Moviein' className='w-[120px]' />
                 </div>
                 <div className='mb-20'>
                   <h2 className='text-4xl mb-2 text-white'>Fazer login</h2>
-                  <p className='text-white'>Novo usuário?
-                    <a onClick={() => nav("/registro")} className='text-primary ml-1 cursor-pointer underline  decoration-slice'>
-                      Crie uma conta
-                    </a>
-                  </p>
+                  <div className='flex gap-1'>
+                    <p className='text-white'>Novo usuário?</p>
+                    <p onClick={() => nav("/registro")} className='text-primary ml-1 cursor-pointer underline  decoration-slice'>Crie uma conta</p>
+                  </div>
                 </div>
                 <form>
                   <Input Icon={<MdEmail />} Nome='Email' Type='email' />
@@ -35,12 +34,13 @@ const Login: React.FC = () => {
                     <input type='submit' value="Entrar" className='w-full cursor-pointer py-3 px-6 text-white bg-primary rounded-lg ' />
                   </div>
                 </form>
-                <div className='mt-6'>
-                  <p className='text-white'>Esqueceu sua senha? <a className='text-primary cursor-pointer underline decoration-slice'>Redefine aqui</a></p>
+                <div className='mt-6 flex gap-1'>
+                  <p className='text-white'>Esqueceu sua senha?</p>
+                  <p className='text-primary cursor-pointer underline decoration-slice'>Redefine aqui</p>
                 </div>
 
                 <div className='mt-6'>
-                  <a className='text-white cursor-pointer underline decoration-slice absolute bottom-4 right-4'>Termos de uso</a>
+                  <p className='text-white cursor-pointer underline decoration-slice absolute bottom-4 right-4'>Termos de uso</p>
                 </div>
               </div>
             </div>
