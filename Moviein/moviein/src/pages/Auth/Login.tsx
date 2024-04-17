00808844d120b7536d3fa018a42786988076f9a6
Eduardo Oliveira <eduardolimaoliveira@souunisuam.com.br>
2024-04-17 07:33:30 -0300

    Update Login.tsx

--- Moviein/moviein/src/pages/Auth/Login.tsx
+++ Moviein/moviein/src/pages/Auth/Login.tsx
@@ -4,7 +4,7 @@ import filmes from '../../assets/filme.png';
 const Login: React.FC = () => {
   return (
     <div className='relative w-full h-screen'>
-      <img src={filmes} className='absolute w-full h-full object-cover -z-10' />
+      <img src={filmes} alt='Filmes' className='absolute w-full h-full object-cover -z-10' />
       <div className='container mx-auto'>
 
         <div className='grid grid-cols-2 h-screen'>
