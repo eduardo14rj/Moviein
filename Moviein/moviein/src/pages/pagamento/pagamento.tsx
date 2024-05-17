d54da7558f15ebb68c8aca93548a3f75dac3de8e
GABRIEL-NOT-FOUND <gabrielpessanha@souunisuam.com.br>
2024-05-16 23:49:30 -0300

    payment begins

--- Moviein/moviein/src/pages/pagamento/pagamento.tsx
+++ Moviein/moviein/src/pages/pagamento/pagamento.tsx
@@ -9,18 +9,9 @@ const pagamento: React.FC = () => {
 <div>
     <div className='relative w-full h-screen' >
         <div className='container padding-top'>
-          <div className='grid-container-3'>
+          <div className='grid-container'>
             <div className='container'>
 
-              <button className='' type='button' title='viewer'>14,00$</button>
-            </div>
-
-            <div className='container'>
-              <button type='button' title='critic'>16,00$</button>
-            </div>
-
-            <div className='container'>
-              <button type='button' title='developer'>18,00$</button>
             </div>
           </div>
         </div>
