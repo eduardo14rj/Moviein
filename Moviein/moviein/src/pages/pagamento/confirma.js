e7a35d3cb9d82360ac7381d92b147743ca2c821c
GABRIEL-NOT-FOUND <gabrielpessanha@souunisuam.com.br>
2024-05-22 00:21:39 -0300

    edição

---
+++ Moviein/moviein/src/pages/pagamento/confirma.js
@@ -0,0 +1,8 @@
+function handlePrint() {
+    window.print();
+}
+
+function handleNewPayment() {
+    // Redirecionar para a página de novo pagamento ou executar outra ação 
+    window.location.href = 'pagina_de_novo_pagamento.html'; // ajuste o URL conforme necessário
+}
